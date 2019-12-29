60b6920b02a2ffcea37512edae50e2ac536a316f
Joe Krump <joe.krump@clio.com>
2019-12-29 14:39:33 -0800

    Remove the inclusion of <script> tag in index which loaded bundle.js

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,24 +8,22 @@ import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 
 interface AppState {
-  socket: SocketIOClient.Socket;
   boardId: string;
+  socket: SocketIOClient.Socket;
 }
 
 export class App extends React.Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
+
     let serverURL = window.location.origin;
     if (!!window.location.port && (window.location.port !== "8000")) {
       serverURL = window.location.origin.replace(window.location.port, "8000");
     }
 
-    const socket = io.connect(serverURL);
-    const boardId = window.location.pathname.split("/").pop() || uuid.v4();
-
     this.state = {
-      socket,
-      boardId,
+      socket: io.connect(serverURL),
+      boardId: window.location.pathname.split("/").pop() || uuid.v4(),
     };
   }
 
