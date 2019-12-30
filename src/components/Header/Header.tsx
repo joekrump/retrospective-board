b5e431f65b197e01b66ca23227f5615224589e35
Joe Krump <joe.krump@clio.com>
2019-12-29 18:32:34 -0800

    Add FE code for toggling the show results state of a board

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
 import "./header.css";
 
-export class Header extends React.Component {
+interface HeaderProps {
+  showResults: boolean;
+  socket: SocketIOClient.Socket;
+  boardId: string;
+}
+
+export class Header extends React.Component<HeaderProps> {
+
+
+  toggleShowResults(e: React.MouseEvent) {
+    e.preventDefault();
+    // emit an event to show results.
+    this.props.socket.emit(`board:show-results:${this.props.boardId}`);
+  }
+
   render() {
     return (
       <header>
@@ -10,7 +24,9 @@ export class Header extends React.Component {
             <div className="text">Retro</div>
           </div>
           <div id="app-controls">
-            <button>Show Results</button>
+            <button onClick={(e) => this.toggleShowResults(e)}>
+              { this.props.showResults ? "Hide Results" : "Show Results"}
+            </button>
           </div>
         </div>
       </header>
