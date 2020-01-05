5f44965bc6b9179c6b8166272dabccaa75a38b43
Joe Krump <joe.krump@clio.com>
2020-01-04 17:05:24 -0800

    Add a stylesheet for the delete button

--- src/components/ButtonDelete/ButtonDelete.tsx
+++ src/components/ButtonDelete/ButtonDelete.tsx
@@ -1,10 +1,10 @@
 import React, { MouseEvent, ReactText } from "react";
-
+import "./button-delete.css";
 interface ButtonDeleteProps {
   id: ReactText;
   handleClick: (event: MouseEvent, id: ReactText) => void;
 }
 
 export const ButtonDelete = (props: ButtonDeleteProps) => (
-  <span onClick={event => props.handleClick(event, props.id)}>🗑️</span>
+  <span className="button--delete" onClick={event => props.handleClick(event, props.id)}>🗑️</span>
 );
