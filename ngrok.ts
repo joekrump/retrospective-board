ee29b50b1c4027215d72c168e0720010eaa35caa
Joe Krump <joe.krump@clio.com>
2020-02-20 21:59:01 -0800

    Install "open" as a dependency and us it to open the ngrok url automatically when starting prod

--- ngrok.ts
+++ ngrok.ts
@@ -2,6 +2,7 @@ import ngrok from "ngrok";
 
 if(process.env.NODE_ENV === "production") {
   (async function() {
+    const open = require("open");
     const url = await ngrok.connect({
       proto: 'http',
       addr: 8000,
@@ -9,5 +10,14 @@ if(process.env.NODE_ENV === "production") {
 
     console.log('Tunnel Created -> \x1b[34m%s\x1b[0m', url);
     console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
+
+    // Opens the URL in the default browser.
+    try {
+      await open(url);
+    } catch {
+      console.warn(
+        `Unable to open "${url}" in browser.`
+      );
+    }
   })();
 }
