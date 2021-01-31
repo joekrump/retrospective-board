509fb9107f3fa6d4f9084b1813dfa237f3c30b7b
Joseph Krump <krump.joseph@gmail.com>
2021-01-31 09:57:16 -0800

    Add it blocks for test cases to be added

--- __tests__/cypress/active-retro.e2e.ts
+++ __tests__/cypress/active-retro.e2e.ts
@@ -1,3 +1,44 @@
 describe("Participating in an active retro", () => {
 
+  it("allows a new card to be added to a column", () => {
+    // TODO: tests to add:
+
+    // The card can contain bold text (markdown format)
+
+    // The card can contain italisized text (markdown format)
+
+    // The card can contain an image (markdown format)
+  });
+
+  it("allows a card to be dragged and dropped to a new column by its owner", () => {
+
+  });
+
+  it("allows a card to be edited by its owner", () => {
+
+  });
+
+  it("prevents a card being edited by anyone other than its owner", () => {
+
+  });
+
+  it("allows stars to be added to a card", () => {
+
+  });
+
+  it("allows for stars to be removed from a card", () => {
+
+  });
+
+  it("allows a new columnn to be added", () => {
+
+  });
+
+  it("allows an existing column to have its name updated", () => {
+
+  });
+
+  it("allows the title of the retro to be updated", () => {
+
+  });
 });
