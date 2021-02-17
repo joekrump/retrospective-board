167efeac933fd557a59b5ce3872d3daac4279992
Joseph Krump <krump.joseph@gmail.com>
2021-02-16 18:25:54 -0800

    Background color of app changes on an interval

--- src/frontend/components/App/App.tsx
+++ src/frontend/components/App/App.tsx
@@ -63,12 +63,46 @@ export const App = () => {
       sessionId,
     });
 
+    const intervalId = updateBackgroundColorIncrementally();
+
     return function cleanup() {
       socket.removeListener(`board:star-limit-reached:${boardId}`);
       socket.removeListener(`board:show-results:${boardId}`);
       socket.close();
+      clearInterval(intervalId);
     };
   }, []);
+
+  function updateBackgroundColorIncrementally() {
+    const tenSeconds = 1000;
+    const initialCSSBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--app--background-color');
+    // remove # prefix from the color string value so that we get an hex number that we can parse.
+    const initialBackgroundColorHexValue = initialCSSBackgroundColor.trim().substr(1);
+    const initialBackgroundColor = parseInt(`0x${initialBackgroundColorHexValue}`, 16);
+    let newBackgroundColor = initialBackgroundColor;
+
+    const intervalId = setInterval(() => {
+      // subtract 15 (0x0F) from each rgb hex value to make the overall color darker.
+      newBackgroundColor -= 0x0F0F0F;
+      // If the integer number of the color is less than 6 characters long (100000 being the smallest 6 character long hex value), then stop making things darker because subracting further will result in an invalid CSS hex color value and the color will switch back to the browser's default background color value.
+      if (newBackgroundColor < 0x100000) {
+        clearInterval(intervalId);
+        return;
+      }
+      updateAppThemeOnTick(newBackgroundColor);
+    }, tenSeconds);
+
+    return intervalId;
+  }
+
+  function updateAppThemeOnTick(newBackgroundColor: number) {
+    const darkGray = 0x707070;
+
+    if (newBackgroundColor < darkGray) {
+      document.documentElement.style.setProperty("--text-color-primary", "#e6e6e6");
+    }
+    document.documentElement.style.setProperty("--app--background-color", `#${newBackgroundColor.toString(16)}`);
+  }
 
   return (
     <>
