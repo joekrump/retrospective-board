1fe8def4484f958449689c12f434325f229dd60a
Joseph Krump <krump.joseph@gmail.com>
2021-02-19 08:25:53 -0800

    Update server.ts
    Replace single quotes with double-quotes

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ let sessionStore: {
 
 const MAX_VOTES_USER_VOTE_PER_BOARD = 10;
 const NEW_BOARD: Board = {
-  title: `Retro - ${(new Date()).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}`,
+  title: `Retro - ${(new Date()).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" })}`,
   showResults: false,
   maxStars: MAX_VOTES_USER_VOTE_PER_BOARD,
   cards: {},
