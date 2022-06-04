591b064e57d456f02af52efb643baa9439de5c7e
Joe Krump <joe.krump@clio.com>
2022-06-03 20:25:11 -0400

    wrap ngrok.connect call in try-catch block

--- ngrok.ts
+++ ngrok.ts
@@ -3,21 +3,25 @@ import ngrok from "ngrok";
 if(process.env.NODE_ENV === "production") {
   (async function() {
     const open = require("open");
-    const url = await ngrok.connect({
-      proto: 'http',
-      addr: 8000,
-    });
-
-    console.log('Tunnel Created -> \x1b[34m%s\x1b[0m', url);
-    console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
-
-    // Opens the URL in the default browser.
     try {
-      await open(url);
-    } catch {
-      console.warn(
-        `Unable to open "${url}" in browser.`
-      );
+      const url = await ngrok.connect({
+        proto: 'http',
+        addr: 8000,
+      });
+
+      console.log('Tunnel Created -> \x1b[34m%s\x1b[0m', url);
+      console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
+
+      // Opens the URL in the default browser.
+      try {
+        await open(url);
+      } catch {
+        console.warn(
+          `Unable to open "${url}" in browser.`
+        );
+      }
+    } catch(e) {
+      console.error(e);
     }
   })();
 }
